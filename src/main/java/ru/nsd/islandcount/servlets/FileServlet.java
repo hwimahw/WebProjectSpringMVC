58f3e1a9cf9646e7b7c7a9ef4a5d5aa8cae962e9
Mansur Irzakhanov <hwimahw@gmail.com>
2020-07-17 17:12:40 +0300

    beans

--- src/main/java/ru/nsd/islandcount/servlets/FileServlet.java
+++ src/main/java/ru/nsd/islandcount/servlets/FileServlet.java
@@ -3,14 +3,14 @@ package ru.nsd.islandcount.servlets;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.context.ContextLoader;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.web.servlet.support.RequestContextUtils;
 import ru.nsd.islandcount.Data;
 import ru.nsd.islandcount.dao.DataDao;
 import ru.nsd.islandcount.objects.Graph;
 import ru.nsd.islandcount.objects.Matrix;
 
-import javax.naming.Context;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,16 +20,18 @@ import java.io.PrintWriter;
 
 public class FileServlet extends HttpServlet {
 
-   ApplicationContext applicationContext = new ClassPathXmlApplicationContext("config.xml");
+    ApplicationContext applicationContext;
     DataDao dataDao;
     Matrix matrix;
     Graph graph;
 
-   public FileServlet(){
-     dataDao = applicationContext.getBean(DataDao.class);
-     matrix = applicationContext.getBean(Matrix.class);
-     graph = applicationContext.getBean(Graph.class);
-   }
+    public FileServlet(){
+        //applicationContext = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+        applicationContext = ContextLoader.getCurrentWebApplicationContext();
+        dataDao = applicationContext.getBean(DataDao.class);
+        matrix = applicationContext.getBean(Matrix.class);
+        graph = applicationContext.getBean(Graph.class);
+    }
 
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
